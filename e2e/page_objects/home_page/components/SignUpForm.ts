53d502657e08533ce121ca3f077cdc9fd12b3f04
Yuliia Naumova <naymovaj@gmail.com>
2025-08-10 14:24:09 +0200

    Added POMs and test for  sign_in_form

---
+++ e2e/page_objects/home_page/components/SignUpForm.ts
@@ -0,0 +1,131 @@
+import { expect, Locator, Page } from '@playwright/test';
+import { Component } from '../../abstract_classes/AbstractClasses';
+
+
+
+class Locators {
+    constructor(private container: Locator) { }
+    modalContainer = this.container;
+    title = this.container.getByRole('heading', { name: "Registration" });
+    nameInputField = this.container.getByLabel('Name');
+    lastNameInputField = this.container.getByLabel('Last Name');
+    signupEmail = this.container.getByLabel('Email')
+    signupPassword = this.container.getByLabel('Password')
+    repeatPassword = this.container.getByLabel('Re-enter password')
+    registerButton = this.container.getByRole('button', { name: "Register" })
+    closeButton = this.container.getByRole('button', { name: 'Close' })
+    nameErrorMessage = this.container.getByText("Name required");
+    lastNameErrorMessage = this.container.getByText("Last name required");
+    emailErrorMessage = this.container.getByText("Email required");
+    passwordErrorMessage = this.container.getByText("Password required");
+    reEnterPasswordErrorMessage = this.container.getByText("Re-enter password required");
+
+}
+
+class Actions {
+    constructor(
+        private locators: Locators,
+    ) { }
+
+    async enterName(name: string): Promise<void> {
+        await this.locators.nameInputField.fill(name)
+    }
+
+    async enterLastName(lastName: string): Promise<void> {
+        await this.locators.lastNameInputField.fill(lastName)
+    }
+    async enterEmail(email: string): Promise<void> {
+        await this.locators.signupEmail.fill(email)
+    }
+
+    async enterPassword(password: string): Promise<void> {
+        await this.locators.signupPassword.fill(password)
+    }
+
+    async reEnterPassword(password: string): Promise<void> {
+        await this.locators.repeatPassword.fill(password)
+    }
+
+    async clickRegisterButton(): Promise<void> {
+        await this.locators.registerButton.click()
+    }
+
+    async clickCloseButton(): Promise<void> {
+        await this.locators.closeButton.click()
+    }
+
+}
+
+
+
+
+class Assertions {
+    constructor(
+        private locators: Locators,
+    ) { }
+
+
+    async verifyRegisterFormVisible(): Promise<void> {
+        await expect(this.locators.modalContainer).toBeVisible()
+        await expect(this.locators.title).toContainText("Registration")
+    }
+
+    async verifyAllElementsAreVisible(): Promise<void> {
+        await expect(this.locators.nameInputField).toBeVisible();
+        await expect(this.locators.lastNameInputField).toBeVisible();
+        await expect(this.locators.signupEmail).toBeVisible();
+        await expect(this.locators.signupPassword).toBeVisible();
+        await expect(this.locators.repeatPassword).toBeVisible()
+        await expect(this.locators.closeButton).toBeVisible();
+        await expect(this.locators.registerButton).toBeVisible()
+    }
+
+    async verifyRegisterButtonToBeDisabled(): Promise<void> {
+        await expect(this.locators.registerButton).not.toBeEnabled()
+    }
+
+    async verifyNoModalsVisible(page: Page): Promise<void> {
+        await expect(this.locators.modalContainer).toHaveCount(0);
+    }
+    async verifyNameErrorVisible(): Promise<void> {
+        await expect(this.locators.nameErrorMessage).toContainText("Name required")
+    }
+    async verifyLastNameErrorVisible(): Promise<void> {
+        await expect(this.locators.lastNameErrorMessage).toContainText("Last name required")
+    }
+    async verifyEmailErrorVisible(): Promise<void> {
+        await expect(this.locators.emailErrorMessage).toContainText("Email required")
+    }
+
+
+    async verifyPasswordErrorVisible(): Promise<void> {
+        await expect(this.locators.passwordErrorMessage).toContainText("Password required")
+    }
+
+    async verifyReEnterPasswordErrorVisible(): Promise<void> {
+        await expect(this.locators.reEnterPasswordErrorMessage).toContainText("Re-enter password required")
+    }
+
+}
+
+
+export default class SignUpForm extends Component {
+    private formContainer: Locator;
+    locators: Locators;
+    do: Actions;
+    check: Assertions;
+    constructor(page: Page, container: Locator) {
+        super(page, container);
+        this.formContainer = page.getByRole('dialog');
+        this.locators = new Locators(this.formContainer);
+        this.do = new Actions(this.locators);
+        this.check = new Assertions(this.locators);
+    }
+    expectLoaded(): Promise<void> {
+        return Promise.resolve();
+    }
+}
+
+
+
+
---
+++ e2e/tests/sighn_in_form.spec.ts
@@ -0,0 +1,78 @@
+import { test, expect } from "@playwright/test"
+import HomePage from "../page_objects/home_page/HomePage"
+import SignInForm from "../page_objects/home_page/components/SignInForm";
+import RestoreAccessForm from "../page_objects/home_page/components/RestoreAccessForm"
+import SignUpForm from "../page_objects/home_page/components/SignUpForm"
+
+
+
+test.describe('Sign in form', async () => {
+    let homePage: HomePage;
+    let signInForm: SignInForm;
+
+
+
+    test.beforeEach(async ({ page }) => {
+
+        homePage = new HomePage(page);
+        await homePage.open()
+        await homePage.expectLoaded()
+        await homePage.do.clickSignInButton()
+        signInForm = new SignInForm(page, homePage.locators.loginModal)
+    })
+
+    test.afterEach(async ({ page }) => {
+        //await signInForm.do.closeAnyOpenModal()
+        await signInForm.check.verifyNoModalsVisible(page)
+    });
+
+    test.only('Validates  visibility and functionality of elements, navigation actions in  "Sign in" Form', async ({ page }) => {
+        await test.step('1: Validate form is visible', async () => {
+            await signInForm.check.verifyFormIsVisible()
+        })
+
+
+        await test.step('2: Validate all elements are visible', async () => {
+            await signInForm.check.verifyAllElementsAreVisible()
+
+        })
+
+        await test.step('3: Login button disabled by default', async () => {
+            await signInForm.check.verifyLoginButtonToBeDisabled()
+        })
+
+        // await test.step('4: Login button enabled after filling fields', async () => {
+
+        // });
+
+
+        await test.step('5: Close modal and check it disappears', async () => {
+            await signInForm.do.clickCloseButton()
+            await signInForm.check.verifyFormIsClosed()
+        })
+
+
+        await test.step('6: Forgot password opens Restore Access modal', async () => {
+            await homePage.do.clickSignInButton()
+            await signInForm.do.clickForgotPasswordButton()
+            await homePage.check.verifyRestoreFormVisible()
+            let restoreAccessForm = new RestoreAccessForm(page, homePage.locators.restoreAccessModal)
+            await restoreAccessForm.do.clickCloseButton()
+            await restoreAccessForm.check.verifyFormIsClosed()
+
+
+
+
+        })
+        await test.step('7: Registration button opens Registration modal', async () => {
+            await homePage.do.clickSignInButton()
+            await signInForm.do.openRegistration()
+            await homePage.check.verifyRegistrationFormVisible()
+            let registrationForm = new SignInForm(page, homePage.locators.registrationModal)
+            await registrationForm.do.clickCloseButton()
+            await registrationForm.check.verifyFormIsClosed()
+
+        });
+
+    })
+})
